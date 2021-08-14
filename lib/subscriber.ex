58d1ad562de5d52a4228cb6dcd2b98985ae3dd3e
brandaoplaster <brandaoplaster@gmail.com>
2021-08-14 15:40:29 -0300

    chore: Adding subscriber search function

--- lib/subscriber.ex
+++ lib/subscriber.ex
@@ -3,10 +3,23 @@ defmodule Subscriber do
 
   @subscriber %{:prepaid => "prepaid.txt", :postpaid => "postpaid.txt"}
 
-  def register(name, number, cpf, plan) do
-    read(plan) ++ [%__MODULE__{name: name, number: number, cpf: cpf, plan: plan}]
-    |> :erlang.term_to_binary()
-    |> write(plan)
+  def register(name, number, cpf, plan \\ :prepaid) do
+    case search_subscriber(number) do
+      nil ->
+        (read(plan) ++ [%__MODULE__{name: name, number: number, cpf: cpf, plan: plan}])
+        |> :erlang.term_to_binary()
+        |> write(plan)
+
+        {:ok, "#{name} subscriber successfully registered"}
+
+      _subscriber ->
+        {:error, "Subscriber number already exists"}
+    end
+  end
+
+  def search_subscriber(number) do
+    (read(:prepaid) ++ read(:postpaid))
+    |> Enum.find(fn subscriber -> subscriber.number == number end)
   end
 
   defp write(subscriber_list, plan) do
