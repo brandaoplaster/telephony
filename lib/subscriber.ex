af47c46545ed1d310c80485e71170d1050611c10
brandaoplaster <brandaoplaster@gmail.com>
2021-08-13 23:58:01 -0300

    chore: Adding functions for reading and writing files

--- lib/subscriber.ex
+++ lib/subscriber.ex
@@ -1,7 +1,22 @@
 defmodule Subscriber do
   defstruct name: nil, number: nil, cpf: nil
 
+  @subscriber "subscriber.txt"
+
   def register(name, number, cpf) do
-    %__MODULE__{name: name, number: number, cpf: cpf}
+    read() ++ [%__MODULE__{name: name, number: number, cpf: cpf}]
+    |> :erlang.term_to_binary()
+    |> write()
+  end
+
+  defp write(subscriber_list) do
+    File.write(@subscriber, subscriber_list)
+  end
+
+  defp read() do
+    {:ok, subscriber} = File.read(@subscriber)
+
+    subscriber
+    |> :erlang.binary_to_term()
   end
 end
