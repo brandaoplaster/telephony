1e8d1aab127af4dde6557753b2be43f8d0da870b
brandaoplaster <brandaoplaster@gmail.com>
2021-08-16 22:18:05 -0300

    feat: Adding function to start files

--- lib/telephony.ex
+++ lib/telephony.ex
@@ -1,5 +1,10 @@
 defmodule Telephony do
 
+  def start do
+    File.write!("postpaid.txt", :erlang.term_to_binary([]))
+    File.write!("prepaid.txt", :erlang.term_to_binary([]))
+  end
+
   def register_subscriber(name, number, cpf, plan) do
     Subscriber.register(name, number, cpf, plan)
   end
