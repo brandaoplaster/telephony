ad768743970f938d975c8f4481be384d07cf90c9
brandaoplaster <brandaoplaster@gmail.com>
2021-08-17 23:31:40 -0300

    chore: [WIP] in makeCall module

--- lib/make_call.ex
+++ lib/make_call.ex
@@ -3,6 +3,10 @@ defmodule MakeCall do
   defstruct date: nil, duration: nil
 
   def register(subscriber, date, plan) do
+    subscriber_updated = %Subscriber{
+      subscriber | calls: subscriber.calls ++ [%__MODULE__{date: date, duration: duration}]
+    }
 
+    subscriber = Subscriber.update(subscriber.number, subscriber_updated)
   end
 end
