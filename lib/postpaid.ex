c7afca7b66f6f1f1b04a7ba693cc1ca2b91350a4
brandaoplaster <brandaoplaster@gmail.com>
2021-08-19 23:52:15 -0300

    feat: Adding make_call and print_account functions from the postpaid module

--- lib/postpaid.ex
+++ lib/postpaid.ex
@@ -1,3 +1,23 @@
 defmodule Postpaid do
-  defstruct value: nil
+  defstruct value: 0
+
+  @minute_cost 1.40
+
+  def make_call(number, date, duration) do
+    Subscriber.search_subscriber(number, :postpaid)
+    |> MakeCall.register(date, duration)
+
+    {:ok, "Call made successfully! duration of #{duration} minutes"}
+  end
+
+  def print_account(month, year, number) do
+    subscriber = Accounts.print_out(month, year, number, :postpaid)
+
+    amount =
+      subscriber
+      |> Enum.map(&(&1.duration * @minute_cost))
+      |> Enum.sum()
+
+    %Subscriber{subscriber | plan: %__MODULE__{value: amount}}
+  end
 end
