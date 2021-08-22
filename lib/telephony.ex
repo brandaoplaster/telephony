f498f84cdd148a9820b5be9d51dcd719cfac1c51
brandaoplaster <brandaoplaster@gmail.com>
2021-08-21 22:51:43 -0300

    feat: Listing subscriber in the telephony module

--- lib/telephony.ex
+++ lib/telephony.ex
@@ -1,5 +1,4 @@
 defmodule Telephony do
-
   def start do
     File.write!("postpaid.txt", :erlang.term_to_binary([]))
     File.write!("prepaid.txt", :erlang.term_to_binary([]))
@@ -8,4 +7,10 @@ defmodule Telephony do
   def register_subscriber(name, number, cpf, plan) do
     Subscriber.register(name, number, cpf, plan)
   end
+
+  def subscriber_list, do: Subscriber.all_subscribers()
+
+  def prepaid_subscriber_list, do: Subscriber.prepaid_subscribers()
+
+  def postpaid_subscriber_list, do: Subscriber.postpaid_subscribers()
 end
