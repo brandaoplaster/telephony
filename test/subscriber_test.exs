c1e2c96c27e9b9c61c75123a84a26f849d9c7122
brandaoplaster <brandaoplaster@gmail.com>
2021-08-23 23:29:15 -0300

    test: Testing subscriber registration

--- test/subscriber_test.exs
+++ test/subscriber_test.exs
@@ -3,20 +3,32 @@ defmodule SubscriberTest do
   doctest Subscriber
 
   setup do
-    File.write("prepaid_test.txt", :erlang.term_to_binary([]))
-    File.write("postpaid_test.txt", :erlang.term_to_binary([]))
+    File.write("prepaid.txt", :erlang.term_to_binary([]))
+    File.write("postpaid.txt", :erlang.term_to_binary([]))
 
     on_exit(fn ->
-      File.rm("prepaid_test.txt")
-      File.rm("postpaid_test.txt")
+      File.rm("prepaid.txt")
+      File.rm("postpaid.txt")
     end)
   end
 
-  describe "tests to register subscriber" do
+  describe "testing subscriber registration" do
     test "when to register a prepaid account" do
-      subscriber = Subscriber.register("Lucas", "12345", "123456")
+      subscriber = Subscriber.register("Lucas", "12345", "123456", :prepaid)
+      expected_response = {:ok, "Lucas subscriber successfully registered"}
+      assert subscriber == expected_response
+    end
 
-      assert subscriber == {:ok, "Lucas subscriber successfully registered"}
+    test "when to register a postpaid account" do
+      subscriber = Subscriber.register("Brandao", "09876", "09876", :postpaid)
+      expected_response = {:ok, "Brandao subscriber successfully registered"}
+      assert subscriber == expected_response
+    end
+
+    test "When the subscriber structure returns" do
+      subscriber =
+        %Subscriber{name: "Brandao", number: "0987654", cpf: "0123456", plan: "prepaid"}.name ==
+          "Brandao"
     end
   end
 end
